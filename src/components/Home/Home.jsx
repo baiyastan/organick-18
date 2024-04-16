75cbe11a46cebdcf172622e682102be1759cf3da
jm1a <borubaevaasiya@gmail.com>
2024-04-16 16:21:12 +0600

    SectionTwo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import SectionOne from '../SectionOne/SectionOne'
+import SectionTwo from '../SectionTwo/SectionTwo'
 
 function Home() {
   return (
     <div>
       <SectionOne/>
-      
+      <SectionTwo/> 
     </div>
   )
 }
