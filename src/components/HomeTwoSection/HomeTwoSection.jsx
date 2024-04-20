d6ba2f8607e15d61a5685c9cd86529234156d198
jm1a <borubaevaasiya@gmail.com>
2024-04-20 21:51:46 +0600

    UseSectionTwo

--- src/components/HomeTwoSection/HomeTwoSection.jsx
+++ src/components/HomeTwoSection/HomeTwoSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import imageLogo from '../../assets/images/imagelogo.png'
-import searchLogo from '../../assets/images/search.png'
+import imageLogo from '../../assets/Images/imagelogo.png'
+import searchLogo from '../../assets/Images/search.png'
 import './HomeTwoSection.css'
 
 function HomeTwoSection() {
@@ -8,6 +8,7 @@ function HomeTwoSection() {
     <section className='home-section'>
         <div className="image-position">
             <img src={imageLogo} alt="" />
+            {/* <img src="" alt="" /> */}
         </div>
         <div className="pustoy"></div>
 
@@ -15,6 +16,7 @@ function HomeTwoSection() {
             <h1>Enjoy in the best way!</h1>
             <h2>Enjoy our services for your trip anytime</h2>
             <div className="search-image">
+              {/* <img src="" alt="" /> */}
                 <img src={searchLogo} alt="" />
             </div>
 
